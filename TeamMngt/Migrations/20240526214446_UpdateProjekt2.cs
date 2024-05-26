d131f6cbc329884a100e2ca6b61cfacac60a7048
KamilMleczko <kmleczko@student.agh.edu.pl>
2024-05-27 00:30:52 +0200

    Cascade and SetNUll deletes for database

---
+++ TeamMngt/Migrations/20240526214446_UpdateProjekt2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamMngt.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjekt2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ModulProjektu_Projekt_ProjektId",
+                table: "ModulProjektu");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ModulProjektu_Projekt_ProjektId",
+                table: "ModulProjektu",
+                column: "ProjektId",
+                principalTable: "Projekt",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ModulProjektu_Projekt_ProjektId",
+                table: "ModulProjektu");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ModulProjektu_Projekt_ProjektId",
+                table: "ModulProjektu",
+                column: "ProjektId",
+                principalTable: "Projekt",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ TeamMngt/Migrations/20240526222557_UpdateProjekt4.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamMngt.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjekt4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pracownik_Zespol_ZespolId",
+                table: "Pracownik");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Zadanie_ModulProjektu_ModulProjektuId",
+                table: "Zadanie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Zespol_ModulProjektu_ModulProjektuId",
+                table: "Zespol");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pracownik_Zespol_ZespolId",
+                table: "Pracownik",
+                column: "ZespolId",
+                principalTable: "Zespol",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Zadanie_ModulProjektu_ModulProjektuId",
+                table: "Zadanie",
+                column: "ModulProjektuId",
+                principalTable: "ModulProjektu",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Zespol_ModulProjektu_ModulProjektuId",
+                table: "Zespol",
+                column: "ModulProjektuId",
+                principalTable: "ModulProjektu",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pracownik_Zespol_ZespolId",
+                table: "Pracownik");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Zadanie_ModulProjektu_ModulProjektuId",
+                table: "Zadanie");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Zespol_ModulProjektu_ModulProjektuId",
+                table: "Zespol");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pracownik_Zespol_ZespolId",
+                table: "Pracownik",
+                column: "ZespolId",
+                principalTable: "Zespol",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Zadanie_ModulProjektu_ModulProjektuId",
+                table: "Zadanie",
+                column: "ModulProjektuId",
+                principalTable: "ModulProjektu",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Zespol_ModulProjektu_ModulProjektuId",
+                table: "Zespol",
+                column: "ModulProjektuId",
+                principalTable: "ModulProjektu",
+                principalColumn: "Id");
+        }
+    }
+}
